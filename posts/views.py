ccfa64f860c55183b97b574bb02659e9e31f5a5e
Christian Albrand <Christian.albrandaguirre@cesunbc.edu.mx>
2025-01-07 21:46:22 -0800

    final report assignment 4 BLOG

---
+++ posts/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+# Create your views here.
+from .models import Post
+from django.urls import reverse_lazy
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "author"]
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
